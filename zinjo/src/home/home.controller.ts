a17770ccbfa7060b64c45830b000dea4465b3aa1
Criss Defaz <leadmicriss001@outlook.es>
2019-07-18 23:18:00 -0500

    Login y formulario de registro

--- zinjo/src/home/home.controller.ts
+++ zinjo/src/home/home.controller.ts
@@ -1,19 +1,70 @@
-import {Controller, Get, Res} from '@nestjs/common';
-import {HomeService} from "./home.service";
+import { Body, Controller, Get, Post, Res, Session } from '@nestjs/common';
+import {HomeService} from './home.service';
 
-
-@Controller()
+@Controller("/home")
 export class HomeController {
     constructor(private readonly inicioService: HomeService) {}
 
-    @Get()
-    getHello(): string {
-        return this.inicioService.getHello();
+    @Get('index')
+    mostrarHome(@Res() res) {
+        res.render('home/index');
     }
 
-    @Get('home')
-    mostrarInicio(@Res() res) {
-        res.render('home/home')
+    @Get('principal')
+    mostrarInicio(@Session() session, @Res() res) {
+        if (session.username) {
+            res.render('home/principal', {
+                nombre: session.username.toUpperCase()
+            });
+        } else {
+            res.redirect('/home/index');
+        }
+    }
+
+    /*
+    @Get('login')
+    loginVista(@Res() res) {
+        res.render('login');
+    }
+     */
+
+    @Post('login')
+    login(@Body() usuario, @Session() session, @Res() res ) {
+        if (usuario.username === 'criss' && usuario.password === 'criss') {
+            session.username = usuario.username;
+            console.log(usuario);
+            res.redirect('/home/principal');
+        }else{
+            res.status(400);
+            res.send({mensaje:'Usuario No registrado',error:400})
+        }
+    }
+
+    @Get('logout')
+    logout(
+      @Res() res,
+      @Session() session,
+    ) {
+        session.username = undefined;
+        session.destroy();
+        res.redirect('/home/index');
+    }
+
+  @Get('registro')
+  registrar(@Res() res) {
+    res.render('form_registro/registro');
+  }
+
+
+  @Get('perfil')
+  mostrarPerfil(@Session() session, @Res() res) {
+        if (session.username) {
+            res.render('perfil/perfil-usuario', {
+                nombre: session.username.toUpperCase()
+            });
+        } else {
+            res.redirect('/home/principal');
+        }
     }
 
 }
