e6b5d020b622021d9caee8dae3102c39fe92274d
iperez <israel-03j@hotmail.com>
2019-07-28 18:13:59 -0500

    añadido editar vestido

--- zinjo/src/vestido/vestido.controller.ts
+++ zinjo/src/vestido/vestido.controller.ts
@@ -1,34 +1,51 @@
-import {Body, Controller, UseInterceptors, UploadedFile, Get, Post, Res, Session, Param} from "@nestjs/common";
-import { FileInterceptor } from  '@nestjs/platform-express';
+import {Body, Controller, Get, Param, Post, Res, Session, UseInterceptors} from "@nestjs/common";
+import {FileInterceptor} from '@nestjs/platform-express';
 import {Vestido, VestidoService} from "./vestido.service";
 import {storage} from '../uploader/uploader';
+import {UsuarioService} from '../usuario/usuario.service';
+import {Usuario} from "../../interfaces/usuario";
 
 @Controller('/vestido')
 export class VestidoController {
     constructor(
-        private readonly _vestidoService:VestidoService,
-    )
-    {}
+        private readonly _vestidoService: VestidoService,
+        private readonly _usuarioService: UsuarioService
+    ) {
+    }
 
-    @Get('registrar')
-    registrarVestido(
+    @Get('registrar/:username')
+    async registrarVestido(
         @Res() response,
         @Session() session,
-        @Param('idUsuario') idUsuario: number
-    )
-    {
-        response.render(
-            'vestido/vestido_registro',
+        @Param('username') username: string
+    ) {
+        this._usuarioService.buscar(
             {
-
-                username: session.username.toUpperCase()
-
-            }
-        )
+                select: ['idUsuario', 'nombreCompleto', 'nombreCompleto', 'direccion',
+                    'fechaNac', 'usuario', 'telefono'],
+                where: [{'usuario': username}],
+            },
+        ).then(
+            value => {
+                value.forEach(
+                    i => {
+                        // console.log(i.idUsuario);
+                        response.render('vestido/vestido_registro',
+                            {valor: i, username: session.username.toUpperCase()});
+                    },
+                );
+            },
+        ).catch(
+            reason => {
+                console.log(reason);
+                response.status(400);
+                response.send({mensaje: 'Error de consulta', error: 400});
+            },
+        );
     }
 
 
-    @Post('crear')@UseInterceptors(
+    @Post('crear/:idUsuario') @UseInterceptors(
         FileInterceptor(
             'pic',
             {
@@ -39,17 +56,63 @@ export class VestidoController {
     async registrarVestidoPost(
         @Res() response,
         @Session() session,
+        @Param('idUsuario') idUsuario,
         @Body() vestido: Vestido,
-    ){
-
+    ) {
+        vestido.usuario = idUsuario
         const vestido_nuevo = await this._vestidoService.crear(vestido);
         console.log(vestido_nuevo);
     }
 
     @Get('listar')
-    async getRoles(
-    ) {
+    async getVestidos() {
         return await this._vestidoService.buscar();
+    }
+
+    @Get('editar/:idvestido')
+    editarUsuario(@Res() res, @Param('idvestido') idvestido: string, @Session() session) {
+        this._vestidoService.buscar(
+            {
+                select: ['id', 'talla', 'color', 'estado','precio',
+                    'descripcion'],
+                where: [{'id': idvestido}],
+            },
+        ).then(
+            value => {
+                value.forEach(
+                    i => {
+                        // console.log(i.idUsuario);
+                        res.render('vestido/vestido_editar',
+                            {valor: i, username: session.username.toUpperCase()});
+                    },
+                );
+            },
+        ).catch(
+            reason => {
+                console.log(reason);
+                res.status(400);
+                res.send({mensaje: 'Error de consulta', error: 400});
+            },);
+    }
+
+
+
+
+    @Post('editar/:idVestido')
+    editarUsuarioPost(@Res() res, @Body() vestido: Vestido,
+                      @Param('idVestido') idVestido: number, @Session() session) {
+        this._vestidoService.actualizarVestido(idVestido, vestido)
+            .then(value => {
+                console.log(value);
+                console.log('Vestido actualizado');
+                // Actualizando usuario de sesion
+
+            })
+            .catch(reason => {
+                console.log(reason);
+                res.status(400);
+                res.send({ mensaje: 'Error actualizando', error: 400 });
+            });
     }
 
 
--- zinjo/src/vestido/vestido.service.ts
+++ zinjo/src/vestido/vestido.service.ts
@@ -4,6 +4,7 @@ import {FindManyOptions, Repository} from "typeorm";
 
 import {InjectRepository} from '@nestjs/typeorm';
 import {UsuarioEntity} from "../usuario/usuario.entity";
+import {Usuario} from "../../interfaces/usuario";
 
 @Injectable()
 export class VestidoService {
@@ -28,20 +29,31 @@ export class VestidoService {
         return vestidoCreado;
     }
 
-    buscar(parametros?:FindManyOptions):Promise<VestidoEntity[]>{
+    buscar(parametros?: FindManyOptions): Promise<VestidoEntity[]> {
         return this._vestidoRepository.find(parametros)
+    }
+
+    async actualizarVestido(id: number, vestido: Vestido) {
+        return await this._vestidoRepository
+            .update(id, {
+                talla: vestido.talla,
+                color: vestido.color,
+                precio: vestido.precio,
+                estado: vestido.estado,
+                descripcion: vestido.descripcion
+            });
     }
 
 
 }
 
 export interface Vestido {
-    id?:number;
-    talla:string;
-    color:string;
-    precio:number;
-    estado:string;
-    descripcion:string;
-    usuario?:UsuarioEntity,
+    id?: number;
+    talla: string;
+    color: string;
+    precio: number;
+    estado: string;
+    descripcion: string;
+    usuario?: UsuarioEntity,
 
 }
